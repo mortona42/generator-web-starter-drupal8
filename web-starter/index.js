b7c9d2546d089c3a4fd467db8938f3894fec20bb
Andrew Morton <mortona42@gmail.com>
2017-02-21 18:34:01 -0800

    Fix broken drupal 8 generator

--- web-starter/index.js
+++ web-starter/index.js
@@ -9,14 +9,14 @@ var generators = require('yeoman-generator'),
 
 module.exports = generators.Base.extend({
   initializing : {
-    async : function() {
-      ygp(this);
-      this.options.addDevDependency(pkg.name, '~' + pkg.version);
-    },
-    platform : function() {
-      // Set the platform
-      this.options.parent.answers.platform = 'drupal8';
-    }
+  //   async : function() {
+  //     ygp(this);
+  //     this.options.addDevDependency(pkg.name, '~' + pkg.version);
+  //   },
+  //   platform : function() {
+  //     // Set the platform
+  //     this.options.parent.answers.platform = 'drupal8';
+  //   }
   },
   prompting : function() {
     var that = this;
@@ -42,25 +42,26 @@ module.exports = generators.Base.extend({
       return Promise.resolve(tags);
     })
     .then(function(tags) {
-      return that.prompt([{
-        type : 'list',
-        name : 'drupal_version',
-        choices : tags,
-        message : 'Select a version of Drupal',
-        default : config.drupal_version,
-      },
-      {
-        type: 'confirm',
-        name: 'features',
-        message: 'Does it use the Features module?',
-        default: config.features,
-      },
-      {
-        type: 'input',
-        name: 'drupal_theme',
-        message: 'Theme name (machine name)',
-        default: config.drupal_theme,
-      },
+      return that.prompt([
+      //     {
+      //   type : 'list',
+      //   name : 'drupal_version',
+      //   choices : tags,
+      //   message : 'Select a version of Drupal',
+      //   default : config.drupal_version,
+      // },
+      // {
+      //   type: 'confirm',
+      //   name: 'features',
+      //   message: 'Does it use the Features module?',
+      //   default: config.features,
+      // },
+      // {
+      //   type: 'input',
+      //   name: 'drupal_theme',
+      //   message: 'Theme name (machine name)',
+      //   default: config.drupal_theme,
+      // },
       {
         type: 'confirm',
         name: 'install_drupal',
@@ -75,62 +76,4 @@ module.exports = generators.Base.extend({
       _.extend(that.options.parent.answers, { 'web-starter-drupal8' : answers });
     });
   },
-  configuring : {
-    addCapistrano : function() {
-      var config = this.config.getAll();
-      
-      // If we're using Capistrano set some additional values
-      if (_.has(this.options.parent.answers, 'web-starter-capistrano')) {
-        _.extend(this.options.parent.answers['web-starter-capistrano'].config, {
-          drupal_features : config.features,
-          drupal_db_updates : 'true',
-          linked_dirs : '%w[public/sites/default/files]'
-        });
-      }
-    },
-    addSolr : function() {
-      // Set local variable for Solr if the user has selected to use Puppet
-      this.options.parent.answers['web-starter-drupal8'].solr = (_.has(this.options.parent.answers, 'web-starter-puppet')) ? this.options.parent.answers['web-starter-puppet'].solr : false;
-    },
-    setThemePath : function() {
-      this.options.parent.answers.theme_path = 'public/themes/' + this.options.parent.answers['web-starter-drupal8'].drupal_theme;
-      this.options.parent.answers.build_path = 'public/themes/' + this.options.parent.answers['web-starter-drupal8'].drupal_theme;
-    }
-  },
-  writing : {
-    drupal : function() {
-      var that = this;
-      var config = this.config.getAll();
-
-      if (config.install_drupal) {
-        // Create a Promise for remote downloading
-        return this.remoteAsync('drupal', 'drupal', config.drupal_version)
-        .bind({})
-        .then(function(remote) {
-          this.remotePath = remote.cachePath;
-          return glob('**', { cwd : remote.cachePath });
-        })
-        .then(function(files) {
-          var remotePath = this.remotePath;
-          _.each(files, function(file) {
-            that.fs.copy(
-              remotePath + '/' + file,
-              that.destinationPath('public/' + file)
-            );
-          });
-        });
-      }
-    },
-    settings : function() {
-      // Get current system config for this sub-generator
-      var config = this.options.parent.answers['web-starter-drupal8'];
-      _.extend(config, this.options.parent.answers);
-      
-      this.fs.copyTpl(
-        this.templatePath('public/sites/default/settings.vm.php'),
-        this.destinationPath('public/sites/default/settings.vm.php'),
-        config
-      );
-    }
-  }
 });
