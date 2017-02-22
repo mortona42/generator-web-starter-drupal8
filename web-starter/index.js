6b1ee31ce4bd61e49b049003bfd83cb298c9cd25
Andrew Morton <mortona42@gmail.com>
2017-02-22 10:18:55 -0800

    Add writing section

--- web-starter/index.js
+++ web-starter/index.js
@@ -5,7 +5,11 @@ var generators = require('yeoman-generator'),
   glob = Promise.promisify(require('glob')),
   pkg = require('../package.json'),
   ygp = require('yeoman-generator-bluebird'),
-  drupal_modules = require('drupal-modules');
+  drupal_modules = require('drupal-modules'),
+
+  memFs = require('mem-fs'),
+  editor = require('mem-fs-editor');
+
 
 module.exports = generators.Base.extend({
   initializing : {
@@ -37,4 +41,37 @@ module.exports = generators.Base.extend({
       }
     ]);
   },
+  writing :  {
+    drupal_composer : function() {
+      // Get current system config for this sub-generator
+      var config = this.options.parent.answers['web-starter-drupal8'];
+      _.extend(config, this.options.parent.answers);
+
+      var gitignore = this.fs.read(
+        this.templatePath('.gitignore')
+      )
+
+      fs = editor.create(store);
+
+      fs.append(
+        this.destinationPath('.gitignore'),
+        gitignore
+      );
+
+      this.fs.copy(
+        this.templatePath('composer.json'),
+        this.destinationPath('composer.json')
+      );
+
+      this.fs.copy(
+        this.templatePath('drush'),
+        this.destinationPath('drush')
+      );
+
+      this.fs.copy(
+        this.templatePath('scripts'),
+        this.destinationPath('scripts')
+      );
+    }
+  }
 });
