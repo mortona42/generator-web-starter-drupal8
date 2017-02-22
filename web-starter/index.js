db59669fa8488feefd9ffd731a6a56f25b6e9217
Andrew Morton <mortona42@gmail.com>
2017-02-21 19:38:01 -0800

    Strip out prompts

--- web-starter/index.js
+++ web-starter/index.js
@@ -22,58 +22,19 @@ module.exports = generators.Base.extend({
     var that = this;
     
     var config = _.extend({
-      features : true,
-      drupal_theme : '',
-      drupal_version : ''
     }, this.config.getAll());
 
-    return drupal_modules.getLatestMinorVersions('drupal').then(function(releases) {
-      var tags = _.chain(releases)
-        .filter({ version_major : 8 })
-        .map(function(release) {
-          return release.version
-        })
-        .value();
-      
-      if (config.drupal_version && tags[0] != config.drupal_version) {
-        tags.push(config.drupal_version);
-      }
-
-      return Promise.resolve(tags);
-    })
-    .then(function(tags) {
-      return that.prompt([
-          {
-        type : 'list',
-        name : 'drupal_version',
-        choices : tags,
-        message : 'Select a version of Drupal',
-        default : config.drupal_version,
-      },
-      {
-        type: 'confirm',
-        name: 'features',
-        message: 'Does it use the Features module?',
-        default: config.features,
-      },
+    return that.prompt([
       {
         type: 'input',
         name: 'drupal_theme',
         message: 'Theme name (machine name)',
-        default: config.drupal_theme,
       },
       {
         type: 'confirm',
         name: 'install_drupal',
-        message: 'Install a fresh copy of Drupal?',
-        default: false,
-      }]);
-    })
-    .then(function(answers) {
-      that.config.set(answers);
-
-      // Expose the answers on the parent generator
-      _.extend(that.options.parent.answers, { 'web-starter-drupal8' : answers });
-    });
+        message: 'Install Drupal via composer?',
+      }
+    ]);
   },
 });
